bae6984a6ae1e37695ae0a30fbce59cb01802439
Eduard Stancu <eduardstancu@yahoo.com>
2020-05-23 09:35:28 +0300

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>
+  res.json({
+    nume: 'Ionel',
+    prenume: 'Ion',
+    varsta: 23,
+  })
+);
+
+// Define routes
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
