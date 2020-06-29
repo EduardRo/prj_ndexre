491e9ef05669622c257d9a4b788e7811ce8bc1f7
Eduard Stancu <eduardstancu@yahoo.com>
2020-06-29 11:02:09 +0300

    mod AlertState

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
+import uuid from 'uuid';
 import AlertContext from './alertContext';
-import AlertReducer from './alertReducer';
+import alertReducer from './alertReducer';
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AlertState = () => {
@@ -10,7 +11,33 @@ const AlertState = () => {
 
   // Set Alert
 
-  return <div></div>;
+  const setAlert = (msg, type, timeout) => {
+    const id = uuid.v4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id },
+    });
+
+    setTimeout(
+      () =>
+        dispatch({
+          type: REMOVE_ALERT,
+          payload: id,
+        }),
+      timeout
+    );
+  };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert,
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
 };
 
 export default AlertState;
